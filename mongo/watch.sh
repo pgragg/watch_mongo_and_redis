b5e995c3b7a65b58a65c395f8b7bd8c753679cbc
Piper Gragg <piper.gragg@kustomer.com>
2023-03-18 15:50:13 -0400

    Wait for mongo

--- mongo/watch.sh
+++ mongo/watch.sh
@@ -10,7 +10,17 @@ echo "Listening to Mongo on namespace $NAMESPACE"
 MONGO_POD_NAME=mongo-0
 nohup kubectl port-forward $MONGO_POD_NAME  --namespace $NAMESPACE 27017:27017 &
 
-sleep 5
+
+
+while true; do
+  if mongosh --quiet --eval "db.runCommand({ping:1})" >/dev/null; then
+    echo "Connected to MongoDB on port 27017"
+    break
+  else
+    echo "Waiting for MongoDB to become available..."
+    sleep 1
+  fi
+done
 
 cat watch.js | mongosh "mongodb://localhost:27017/?readPreference=primary&appname=$NAMESPACE&directConnection=true&ssl=false&serverSelectionTimeoutMS=10000"
 
